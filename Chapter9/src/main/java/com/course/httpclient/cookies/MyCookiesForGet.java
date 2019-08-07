6dd52c30f3c29fce10ac5298290d7d34d7a2ccf9
zhangxi666 <13166113029.com>
2019-08-07 16:37:40 +0800

    获取cookie

---
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -0,0 +1,54 @@
+/*
+    获取cookies
+
+ */
+package com.course.httpclient.cookies;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MyCookiesForGet {
+    private String url;
+    private ResourceBundle bundle;
+
+    @BeforeTest
+    public void beforeTest(){
+        bundle = ResourceBundle.getBundle("application",Locale.CHINA);
+        url = bundle.getString("test.url");
+
+    }
+    @Test
+    public void testGetCookies() throws IOException {
+        String result;
+        //从配置文件application中拼接测试url
+        String uri = bundle.getString("getCookies.uri");
+        String testUrl =this.url+uri;
+        System.out.println(testUrl);
+        //测试逻辑代码书写
+        HttpGet get = new HttpGet(testUrl);
+        DefaultHttpClient client = new DefaultHttpClient();
+
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+        //获取cookies信息
+        CookieStore store = client.getCookieStore();
+        List<Cookie> cookieList = store.getCookies();
+        //遍历cookie key和value
+        for (Cookie cookie: cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name = " + name + ";"+ "cookie value = "+ value);
+        }
+    }
+}
